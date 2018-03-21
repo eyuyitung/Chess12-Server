c2d1828dda0375e737b08a66a1edd1e1e74f084a
eyuyitung <eyuyitung@gmail.com>
2018-03-21 13:54:42 -0400

    added more piece

--- src/Pawn.java
+++ src/Pawn.java
@@ -1,5 +1,10 @@
-public class Pawn {
-    boolean firstMove;
+public class Pawn extends Piece{
+    boolean firstMove = true;
 
+    boolean canMove (){
+        return board.spaceAhead(this.xPos, this.yPos) == null;
+        if (firstMove)
+            return board.spaceAhead(this.xPos,this.yPos + 1) == null;
+    }
 
 }
